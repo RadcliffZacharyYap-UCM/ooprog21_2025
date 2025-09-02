6b109cf6aae2bd9f2d49da01b80579e2f35b32ff
RadcliffZacharyYap-UCM <radcliffyap69@gmail.com>
2025-09-02 21:41:47 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("I want you to know how deep my love goes");
+        System.out.println("I ain't gotta a lot of time, no speech, just follow");
+        System.out.println("Some tricks in and up my sleeve at the magic show");
+        System.out.println("Got your body on top of me, all around, in and out you go");
+    }
+}
