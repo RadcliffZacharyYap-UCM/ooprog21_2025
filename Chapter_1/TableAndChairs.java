a004a5a17925c289ad4dd2b582c84f9c040abb40
RadcliffZacharyYap-UCM <radcliffyap69@gmail.com>
2025-09-03 21:12:30 +0800

    Create TableAndChairs.java

---
+++ Chapter_1/TableAndChairs.java
@@ -0,0 +1,15 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("X                       X");
+        }
+        
+        System.out.println("X       XXXXXXXXXX      X");
+        
+        for (int i = 0; i < 1; i++) {
+            System.out.println("XXXXX   X        X  XXXXX");
+            System.out.println("X   X   X        X  X   X");
+            System.out.println("X   X   X        X  X   X");
+        }
+    }
+}
