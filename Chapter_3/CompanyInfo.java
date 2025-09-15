40c599e86a5a52ca0c9ab668e82f830b909700e8
RadcliffZacharyYap-UCM <radcliffyap69@gmail.com>
2025-09-15 08:57:36 +0800

    Create CompanyInfo.java

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,13 @@
+public class CompanyInfo {
+
+    public static void main(String[] args) {
+        System.out.println("India's Tech Solutions Inc.");
+        displayHours();
+    }
+
+    public static void displayHours() {
+        System.out.println("Monday - Friday: 8:00 AM - 6:00 PM");
+        System.out.println("Saturday - 8:00 AM to Noon");
+        System.out.println("Sunday - Closed");
+    }
+}
